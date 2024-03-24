9b4b5b31bb3ce737bcd9e6409c22c56e469d960d
Dhrumil Mistry <56185972+dmdhrumilmistry@users.noreply.github.com>
2024-03-24 11:55:58 +0530

    use bgless images

--- app/constants/urls.ts
+++ app/constants/urls.ts
@@ -1,3 +1,5 @@
 export const safeackGithubLogoUrl: string =
     "https://avatars.githubusercontent.com/u/157896088";
 
+export const safeackBGlessImage: string =
+    "/static/logo/bg-less-safeack-logo-500-500px.png";
--- app/ui/dashboard/sidebar/sidebar.tsx
+++ app/ui/dashboard/sidebar/sidebar.tsx
@@ -1,5 +1,118 @@
+import { Box, Image, Stack } from "@chakra-ui/react";
+import {
+    MdDashboard,
+    MdSupervisedUserCircle,
+    MdShoppingBag,
+    MdAttachMoney,
+    MdWork,
+    MdAnalytics,
+    MdPeople,
+    MdOutlineSettings,
+    MdHelpCenter,
+    MdLogout,
+} from "react-icons/md";
+
+const menuItems = [
+    {
+        title: "Pages",
+        list: [
+            {
+                title: "Dashboard",
+                path: "/dashboard",
+                icon: <MdDashboard />,
+            },
+            {
+                title: "Users",
+                path: "/dashboard/users",
+                icon: <MdSupervisedUserCircle />,
+            },
+            {
+                title: "Products",
+                path: "/dashboard/products",
+                icon: <MdShoppingBag />,
+            },
+            {
+                title: "Transactions",
+                path: "/dashboard/transactions",
+                icon: <MdAttachMoney />,
+            },
+        ],
+    },
+    {
+        title: "Analytics",
+        list: [
+            {
+                title: "Revenue",
+                path: "/dashboard/revenue",
+                icon: <MdWork />,
+            },
+            {
+                title: "Reports",
+                path: "/dashboard/reports",
+                icon: <MdAnalytics />,
+            },
+            {
+                title: "Teams",
+                path: "/dashboard/teams",
+                icon: <MdPeople />,
+            },
+        ],
+    },
+    {
+        title: "User",
+        list: [
+            {
+                title: "Settings",
+                path: "/dashboard/settings",
+                icon: <MdOutlineSettings />,
+            },
+            {
+                title: "Help",
+                path: "/dashboard/help",
+                icon: <MdHelpCenter />,
+            },
+        ],
+    },
+];
+
 const Sidebar = () => {
-    return <div>SideBar</div>;
+    return (
+        <Stack>
+            <Box>
+                <Image
+                    src={"/noavatar.png"}
+                    alt=""
+                    width="50"
+                    height="50"
+                />
+                <Box>
+                    <span>{user.username}</span>
+                    <span className={styles.userTitle}>Administrator</span>
+                </Box>
+            </Box>
+            <ul className={styles.list}>
+                {menuItems.map((cat) => (
+                    <li key={cat.title}>
+                        <span className={styles.cat}>{cat.title}</span>
+                        {cat.list.map((item) => (
+                            <MenuLink item={item} key={item.title} />
+                        ))}
+                    </li>
+                ))}
+            </ul>
+            <form
+                action={async () => {
+                    "use server";
+                    await signOut();
+                }}
+            >
+                <button className={styles.logout}>
+                    <MdLogout />
+                    Logout
+                </button>
+            </form>
+        </Stack>
+    );
 };
 
 export default Sidebar;
