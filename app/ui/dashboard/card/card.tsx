36acf52a184c0d1ba29aecda9f4aa29cde64322c
Dhrumil Mistry <56185972+dmdhrumilmistry@users.noreply.github.com>
2024-03-24 20:05:17 +0530

    complete card component

--- app/ui/dashboard/card/card.tsx
+++ app/ui/dashboard/card/card.tsx
@@ -1,25 +1,59 @@
 "use client";
 
-import { Flex, Box, Text, useColorModeValue } from "@chakra-ui/react";
+import { Flex, Text, useColorModeValue } from "@chakra-ui/react";
+import { GoDiff } from "react-icons/go";
 
 // Local imports
 import {
     lightThemeHoverBgColor,
     darkThemeHoverBgColor,
+    lightThemeSideBarBgColor,
+    darkThemeSideBarBgColor,
 } from "../../constants/colors";
 
 const Card: React.FC = () => {
-    const hoverBgColor = useColorModeValue(
+    const cardhoverBgColor = useColorModeValue(
         lightThemeHoverBgColor,
         darkThemeHoverBgColor
     );
+    const cardBgColor = useColorModeValue(
+        lightThemeSideBarBgColor,
+        darkThemeSideBarBgColor
+    );
+
+    // BUG: if user changes theme in browser then they have to reload in order to get latest values of cardhoverBgColor and cardBgColor variables.
 
     return (
         // container
-        <Flex backgroundColor={hoverBgColor}>
-            <Box>
-                <Text></Text>
-            </Box>
+        <Flex
+            backgroundColor={cardBgColor}
+            padding="10px"
+            borderRadius="20px"
+            gap="30px"
+            cursor="pointer"
+            width="100%"
+            _hover={{ backgroundColor: cardhoverBgColor }}
+        >
+            <GoDiff size={25} />
+            {/* Texts */}
+            <Flex flexDirection="column" gap="20px">
+                {/* Title */}
+                <Flex>
+                    <Text>Total Vulnerable Endpoints</Text>
+                </Flex>
+                {/* Number */}
+                <Flex fontSize="24px" fontWeight={500}>
+                    <Text>100</Text>
+                </Flex>
+                {/* Detail */}
+                <Flex fontSize="14px" fontWeight={300}>
+                    {/* Positive or Negative */}
+                    <Flex textColor={"red" || "lime"}>
+                        <Text fontWeight="bold">10%</Text>
+                    </Flex>
+                    <Text>more than last scan</Text>
+                </Flex>
+            </Flex>
         </Flex>
     );
 };
